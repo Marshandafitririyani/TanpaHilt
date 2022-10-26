b0e87ec55a1dc0b9ba1ccb00e64549f8e64c0012
Marshandafitririyani <mfitririyani@gmail.com>
2022-10-26 20:32:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/data/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.data
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.example.data.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels {
+        MainViewModel.Factory(SaveCounter(this))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
+
+        viewModel.sendValue()
+    }
+}
